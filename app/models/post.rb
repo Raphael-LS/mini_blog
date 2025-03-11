7772598c8e4341e864572bb87aac49a1c1ded7f7
Raphael-LS <raphaeltwins@hotmail.com>
2025-03-11 20:26:45 -0300

    Adding validations on Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+    validates :title, :description, presence: true 
+    validates :title, length: { minimum: 2 }
+    validates :description, length: { minimum: 5 }
 end
